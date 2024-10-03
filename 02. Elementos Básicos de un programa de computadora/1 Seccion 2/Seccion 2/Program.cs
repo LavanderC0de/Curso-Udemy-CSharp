cd46c60bad972820d3647b904c0a0ceca3a7e43d
Baeza1320 <david.baeza@lavandercode.es>
2024-10-03 08:42:13 +0200

    EJERCICIO 2
    PRUEBA

---
+++ 02. Elementos Básicos de un programa de computadora/1 Seccion 2/Seccion 2/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seccion_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. 1");
+            Console.WriteLine("2. 2");
+            Console.WriteLine("3. 3");
+            Console.WriteLine("4. 4");
+            Console.WriteLine("5. 5");
+            Console.WriteLine("6. 6");
+
+            // DECLARO VARIABLE DE TIPO ENTERO
+            int numeroLibros, libroNiños;
+
+            //DECLARO UNA VARIABLE DE TIPO DOUBLE Y LE ASIGNO VALOR
+            double promedioFinal = 9.8;
+
+            numeroLibros = 500;// ASIGNO EL VALOR 500 A LA VARIABLE 
+
+            char salon;
+            salon = 'a';
+
+            string saludo = "Hola";
+
+
+            bool x=false;
+
+            decimal promedio= 8.5m; //DECIMAL 
+
+            /* PARA CREAR UN BLOQUE DE COMENTARIOS
+             * HAY QUE PONER PRIMERO LA BARRA LATERAL
+             * Y LUEGO PONERLE EL ASTERISCO Y PARA FINALIZAR
+             * HAY QUE HACER LO MISMO*/
+        }
+    }
+}
