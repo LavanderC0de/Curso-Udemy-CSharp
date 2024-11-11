bca1163ba0fe327864225a7bc3be28b464a8e83a
Vicentee <vicentejuangarcia@gmail.com>
2024-11-11 13:47:59 +0100

    Tarea 4 incompleta 111124

---
+++ 04. Los Ciclos/TAREA 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce la base: ");
+        double baseNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Introduce el exponente: ");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        double result = 1;
+
+        if (exponent == 0)
+        {
+            result = 1;
+        }
+        else if (exponent > 0)
+        {
+            for (int i = 1; i <= exponent; i++)
+            {
+                result *= baseNumber;
+            }
+        }
+        else
+        {
+            for (int i = 1; i <= -exponent; i++)
+            {
+                result *= baseNumber;
+            }
+            result = 1 / result;
+        }
+
+        Console.WriteLine($"Resultado: {result}");
+    }
+}
